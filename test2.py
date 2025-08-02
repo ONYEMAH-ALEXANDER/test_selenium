715c2f475761e7d8012b677d327a52cfda6778de
unknown <onyemah.alexander.chukwueloka@gmail.com>
2025-08-02 17:37:03 +0100

    pushing test 2

---
+++ test2.py
@@ -0,0 +1,8 @@
+class Student:
+    def __init__(self,name,age):
+        self.name = name
+        self.age = age
+
+my_student = Student("Alex",26)
+print(my_student.name) 
+print(my_student.age)   
